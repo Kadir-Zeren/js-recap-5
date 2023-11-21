bd5b42d04e51bb844e73393b492bbfe3f3b94bfd
kadir-zeren <k4dzeren@gmail.com>
2023-11-21 15:48:04 +0300

    v2

--- app.js
+++ app.js
@@ -15,3 +15,22 @@ yazdirParametre("Hello", "2024");
 yazdirParametre("Salut", "2025");
 yazdirParametre("Hola", "2026");
 yazdirParametre("Hallo");
+
+function calculate(name, yob) {
+  const age = new Date().getFullYear() - yob;
+  console.log(`${name} is ${age} years old`);
+  return age;
+}
+console.log(new Date());
+console.log(calculate("Ahmet", 1990));
+const ismetAge = calculate("Ismet", 1890);
+console.log("Ismet's Age:", ismetAge);
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const result1 = add(3, 5);
+const result2 = add(5, 9);
+
+console.log(result1, result2);
